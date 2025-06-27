bab187e021c0e4f7c57763cd914852859fdabca3
16210 <147807760+16210@users.noreply.github.com>
2025-06-27 15:10:25 +0800

    bug修复

--- bash-declare.lisp
+++ bash-declare.lisp
@@ -37,10 +37,44 @@
     (setf var-lst (nconc var-lst (list (list attr name value))))))
 
 (defmacro mk-declare (attribute name value)
-  `(list ,attribute ,name (format nil "~S" ,value)))
+  `(list ,attribute ,name ,value))
 (defmacro declare-attribute (d)
   `(car ,d))
 (defmacro declare-name (d)
   `(cadr ,d))
 (defmacro declare-value (d)
   `(caddr ,d))
+
+(defun mk-declare-scalar (attribute name value)
+  (if value
+    (setf value (format nil "~S" value)))
+  (mk-declare attribute name value))
+(defun mk-declare-array (attribute name value)
+  (mk-declare attribute name value))
+
+(defun declare-value-scalar (d)
+  (let ((v (declare-value d)))
+    (if v
+      (subseq v 1 (1- (length v))))))
+(defun declare-value-array (d)
+  (declare-value d))
+
+(defun set-declare-value-scalar (d value)
+  (if value
+    (setf value (format nil "~S" value)))
+  (setf (declare-value d) value))
+(defun set-declare-value-array (d value)
+  (setf (declare-value d) value))
+
+;; 将 <declare> 列表输出为 bash“declare”命令
+;; 参数：
+;;	liu			输出流
+;;	bash-declare-list	<declare> 列表
+;; 返回值：
+;;	nil
+(defun write-bash-declare (liu bash-declare-list)
+  (dolist (v bash-declare-list)
+    (format liu "declare ~A ~A" (declare-attribute v) (declare-name v))
+    (if (declare-value v)
+      (format liu "=~A~%" (declare-value v))
+      (format liu "~%"))))
