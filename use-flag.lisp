c360dfd1f2e5e8e95265254979bb0c1dfcf39b1e
16210 <147807760+16210@users.noreply.github.com>
2025-12-31 15:44:55 +0800

    组说明符应用标志约束计算函数

--- use-flag.lisp
+++ use-flag.lisp
@@ -182,3 +182,168 @@
       (if match?
 	(<= prev-match 1)
 	(>= (+ prev-match rest-len) 2)))))
+
+;; 计算组说明符的应用标志约束
+;; 参数：
+;;	use+		必须启用的应用标志列表
+;;	use-		必须禁用的应用标志列表
+;;	gtype		<说明符类型>，必须是 :all，:any，:=1 或 :<=1
+;;	prev-len	匹配情况已确定的成员数量
+;;	prev-match	已匹配的成员数量
+;;	rest-list	匹配情况未确定的成员 <说明符*> 列表
+;;	match?		组说明符应当匹配(t)还是不匹配(nil)
+;; 返回值：
+;;	* 成功：
+;;	  1. rest-list 不全忽略情况下必须启用的应用标志列表组成的列表
+;;	  2. rest-list 不全忽略情况下必须禁用的应用标志列表组成的列表
+;;	  二者对应顺序保持一致
+;;	  3. rest-list 为空或全忽略情况下必须启用的应用标志列表组成的列表
+;;	  4. rest-list 为空或全忽略情况下必须禁用的应用标志列表组成的列表
+;;	  二者对应顺序保持一致
+;;	* 应用标志不匹配：
+;;	  1. :not-matched
+;;	  2. 不匹配的标志名称
+;;	* 不满足匹配要求：
+;;	  1. :not-matched
+;;	  [2]. nil
+(defun use-state-constraints (use+ use- gtype prev-len prev-match rest-list match?)
+  (let ((rest-len (length rest-list))
+	next-spec* stype scontent
+	enum-collapse enum-use+ enum-use-
+	next-ig-constraints+ next-ig-constraints-
+	next-nig-constraints+ next-nig-constraints- next-match
+	group-ig-ulist+ group-ig-ulist-
+	group-nig-ulist+ group-nig-ulist- mismatched-flag)
+    (if (zerop rest-len)
+      (if (or (zerop prev-len)
+	      (match-discriminant gtype match? prev-len prev-match 0))
+	(return-from use-state-constraints (values nil nil (list use+) (list use-)))
+	(return-from use-state-constraints :not-matched)))
+    (setf next-spec* (car rest-list)
+	  stype (spec-type next-spec*)
+	  scontent (spec*-content next-spec*))
+
+    (if (spectype-usecond-p stype)
+      (let ((enable? (usecond-enable? stype))
+	    (cond-flag (usecond-flag stype)))
+	(if enable?
+	  (if (member cond-flag use+ :test #'string=)
+	    (setf enum-collapse (list (make-spec* :all scontent))
+		  enum-use+ (list use+)
+		  enum-use- (list use-))
+	    (if (member cond-flag use- :test #'string=)
+	      (setf enum-collapse (list :ignored)
+		    enum-use+ (list use+)
+		    enum-use- (list use-))
+	      (setf enum-collapse (list (make-spec* :all scontent) :ignored)
+		    enum-use+ (list (cons cond-flag use+) use+)
+		    enum-use- (list use- (cons cond-flag use-)))))
+	  (if (member cond-flag use+ :test #'string=)
+	    (setf enum-collapse (list :ignored)
+		  enum-use+ (list use+)
+		  enum-use- (list use-))
+	    (if (member cond-flag use- :test #'string=)
+	      (setf enum-collapse (list (make-spec* :all scontent))
+		    enum-use+ (list use+)
+		    enum-use- (list use-))
+	      (setf enum-collapse (list (make-spec* :all scontent) :ignored)
+		    enum-use+ (list use+ (cons cond-flag use+))
+		    enum-use- (list (cons cond-flag use-) use-))))))
+      (setf enum-collapse (list next-spec*)
+	    enum-use+ (list use+)
+	    enum-use- (list use-)))
+
+    (dotimes (i (length enum-collapse))
+      (setf use+ (nth i enum-use+)
+	    use- (nth i enum-use-))
+      (if (eql (nth i enum-collapse) :ignored)
+	(setf next-ig-constraints+ (nconc next-ig-constraints+ (list (list use+)))
+	      next-ig-constraints- (nconc next-ig-constraints- (list (list use-))))
+	(do ((m nil t)
+	     (nm 0 (1+ nm))
+	     (sub-stype (spec-type (nth i enum-collapse)))
+	     (sub-scontent (spec*-content (nth i enum-collapse)))
+	     (subgroup-ig-not-recorded? t))
+	  ((= nm 2))
+	  (if (eql sub-stype t)
+	    ;; 原子说明符
+	    (if (match-discriminant gtype match? (1+ prev-len) (+ prev-match nm) (1- rest-len))
+	      (let* ((enable? (char/= (char sub-scontent 0) #\!))
+		     (flag (if enable? sub-scontent (subseq sub-scontent 1))))
+		(if (eql enable? m)
+		  (if (member flag use+ :test #'string=)
+		    (setf next-nig-constraints+ (nconc next-nig-constraints+ (list (list use+)))
+			  next-nig-constraints- (nconc next-nig-constraints- (list (list use-)))
+			  next-match (nconc next-match (list nm)))
+		    (if (member flag use- :test #'string=)
+		      (setf mismatched-flag flag)
+		      (setf next-nig-constraints+ (nconc next-nig-constraints+ (list (list (cons flag use+))))
+			    next-nig-constraints- (nconc next-nig-constraints- (list (list use-)))
+			    next-match (nconc next-match (list nm)))))
+		  (if (member flag use- :test #'string=)
+		    (setf next-nig-constraints+ (nconc next-nig-constraints+ (list (list use+)))
+			  next-nig-constraints- (nconc next-nig-constraints- (list (list use-)))
+			  next-match (nconc next-match (list nm)))
+		    (if (member flag use+ :test #'string=)
+		      (setf mismatched-flag flag)
+		      (setf next-nig-constraints+ (nconc next-nig-constraints+ (list (list use+)))
+			    next-nig-constraints- (nconc next-nig-constraints- (list (list (cons flag use-))))
+			    next-match (nconc next-match (list nm))))))))
+	    ;; 组说明符
+	    (multiple-value-bind (nig-ulist+ nig-ulist- ig-ulist+ ig-ulist-) (use-state-constraints use+ use- sub-stype 0 0 sub-scontent m)
+	      (unless (eql nig-ulist+ :not-matched)
+		(if (and nig-ulist+
+			 (match-discriminant gtype match? (1+ prev-len) (+ prev-match nm) (1- rest-len)))
+		  (setf next-nig-constraints+ (nconc next-nig-constraints+ (list nig-ulist+))
+			next-nig-constraints- (nconc next-nig-constraints- (list nig-ulist-))
+			next-match (nconc next-match (list nm))))
+		(if (and ig-ulist+
+			 subgroup-ig-not-recorded?)
+		  (setf next-ig-constraints+ (nconc next-ig-constraints+ (list ig-ulist+))
+			next-ig-constraints- (nconc next-ig-constraints- (list ig-ulist-))
+			subgroup-ig-not-recorded? nil)))
+	      (if (and (eql nig-ulist+ :not-matched)
+		       nig-ulist-)
+		(setf mismatched-flag nig-ulist-)))))))
+
+    (dotimes (i (length next-match))
+      (let ((step-match (nth i next-match))
+	    (step-nig-ulist+ (nth i next-nig-constraints+))
+	    (step-nig-ulist- (nth i next-nig-constraints-)))
+	(dotimes (j (length step-nig-ulist+))
+	  (multiple-value-bind (nig-ulist+ nig-ulist- ig-ulist+ ig-ulist-) (use-state-constraints (nth j step-nig-ulist+)
+												  (nth j step-nig-ulist-)
+												  gtype
+												  (1+ prev-len)
+												  (+ prev-match step-match)
+												  (cdr rest-list)
+												  match?)
+	    (if (eql nig-ulist+ :not-matched)
+	      (if nig-ulist-
+		(setf mismatched-flag nig-ulist-))
+	      (setf group-nig-ulist+ (nconc group-nig-ulist+ nig-ulist+ ig-ulist+)
+		    group-nig-ulist- (nconc group-nig-ulist- nig-ulist- ig-ulist-)))))))
+
+    (dotimes (i (length next-ig-constraints+))
+      (let ((step-ig-ulist+ (nth i next-ig-constraints+))
+	    (step-ig-ulist- (nth i next-ig-constraints-)))
+	(dotimes (j (length step-ig-ulist+))
+	  (multiple-value-bind (nig-ulist+ nig-ulist- ig-ulist+ ig-ulist-) (use-state-constraints (nth j step-ig-ulist+)
+												  (nth j step-ig-ulist-)
+												  gtype
+												  prev-len
+												  prev-match
+												  (cdr rest-list)
+												  match?)
+	    (if (eql nig-ulist+ :not-matched)
+	      (if nig-ulist-
+		(setf mismatched-flag nig-ulist-))
+	      (setf group-ig-ulist+ (nconc group-ig-ulist+ ig-ulist+)
+		    group-ig-ulist- (nconc group-ig-ulist- ig-ulist-)
+		    group-nig-ulist+ (nconc group-nig-ulist+ nig-ulist+)
+		    group-nig-ulist- (nconc group-nig-ulist- nig-ulist-)))))))
+
+    (if (or group-nig-ulist+ group-ig-ulist+)
+      (values group-nig-ulist+ group-nig-ulist-
+	      group-ig-ulist+ group-ig-ulist-)
+      (values :not-matched mismatched-flag))))
